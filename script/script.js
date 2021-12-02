64bfbf93b647a155f86d695a60f0567a62035631
elnurdev <elnuranvarbek@mail.ru>
2021-12-02 19:56:15 +0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,7 @@
+ const hamburger = document.querySelector(".ham");  
+ const navsub = document.querySelector(".nav-sub"); 
+ 
+hamburger.addEventListener('click', () => {  
+    hamburger.classList.toggle("change")  
+    navsub.classList.toggle("nav-change")  
+});  
